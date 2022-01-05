ff38a1db9b5b6efed99e6d08bf481d40b3f1f79e
DiegoCastanedaC <diego.castaneda@gmx.com>
2022-01-04 22:57:25 -0800

    construccion html

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express");
 
 const app = express();
 
+app.use( express.static("public"))
+
+app.get("/home",(req, res) =>{
+    res.sendFile(__dirname + "/views/home.html")
+})
+
+
 app.listen(3000, () =>{
     console.log('servidor corriendo en puerto 3000')
 })
