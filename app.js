007b2dd3ff8b92c4ebc219bf31bff799ef37193a
DiegoCastanedaC <diego.castaneda@gmx.com>
2022-01-13 20:42:55 -0800

    Sections

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
+const path = require('path');
+
 const express = require("express");
 
 const app = express();
 
 app.use( express.static("public"))
 
-app.get("/home",(req, res) =>{
-    res.sendFile(__dirname + "/views/home.html")
+
+app.get("/construccion",(req, res) =>{
+    res.sendFile(path.resolve("./views/construccion.html"))
 })
 
 
-app.listen(3000, () =>{
+app.get("/home",(req, res) =>{
+    res.sendFile(path.resolve("./views/home.html"))
+})
+
+app.get("/tshirts",(req, res) =>{
+    res.sendFile(path.resolve("./views/tshirts.html"))
+})
+
+
+
+app.listen(3030, () =>{
     console.log('servidor corriendo en puerto 3000')
 })
